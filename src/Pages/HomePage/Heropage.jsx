34d9dd56905b45f6665ce5392b6eb6138e246bc3
Dc-Valerian <valerianpedro03@gmail.com>
2025-10-23 15:09:21 +0100

    added

--- src/Pages/HomePage/Heropage.jsx
+++ src/Pages/HomePage/Heropage.jsx
@@ -1,9 +1,54 @@
+import { useState } from "react";
 import CardProps from "../CardProps/CardProps";
 import "../CardProps/CardProps.css";
+
 const Heropage = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const [text, setText] = useState("Hello ✨👋");
+
+  const changeText = () => {
+    setText("Welcome to React 🚀");
+  };
+
+  const [on, setOn] = useState(false);
+
+  const toggle = () => {
+    setOn(!on);
+  };
+
+  const [name, setName] = useState("");
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <div>
-      <h1>This is the hero page</h1>
+      <h1>This is {text}</h1>
+      <button onClick={changeText}>Change Text</button>
+
+      <button onClick={handleIncrement}>Count</button>
+
+      <p>Count is: {counter}</p>
+
+      <div>
+        <h2>The light is {on ? "On" : "Off"}</h2>
+        <button onClick={toggle}>{on ? "Turn Off" : "Turn On"}</button>
+      </div>
+
+      <input
+        type="text"
+        placeholder="Enter your Name"
+        value={name}
+        onChange={handleChange}
+      />
+
+      <p>Hello {name} welcome to my react application</p>
+
       <div className="card-props-container">
         <CardProps
           h3="First Card"
